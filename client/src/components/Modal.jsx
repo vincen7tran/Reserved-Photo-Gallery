fa7f6b8436b1d93e97513dd8afad15b99ae87452
Matt Violet <mattviolet@gmail.com>
2019-05-25 12:37:37 -0700

    Revert files back to commit d32eb6da54635ea9892d5bd176586f628c6a96ab

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,28 +1,23 @@
 import React from 'react';
 
+
 class Modal extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      currentPhoto: this.props.modalImage
+      currentPhoto: 'https://s3-us-west-1.amazonaws.com/asyncphotos/1.jpg'
     };
   }
-
 
   render(){
     return (
       <div id='simpleModal' className='modal'>
         <span onClick={(e) => this.props.closeModal(e)} className='closeBtn'>&times;</span>
-        <div className='modal-content-holder'>
-          <img className='modal-content' src={this.props.modalImage} />
+        <div>
+          <div className='modal-content' id='one' />
           <div className='modal-footer'>
-            <div className='circle-holder'>
-              <p className='circle'>OT</p>
-            </div>
-            <div className='text-holder'>
-              <div className='text'>OpenTable Diner</div>
-              <div className='dinedOn'>Dined on January 3, 2018</div>
-            </div>
+            <div className='circle'>OT</div>
+            <div>OpenTable</div>
           </div>
         </div>
       </div>
